9370435fa31fb1e6d76b237976844c34c74714e3
NataliaLara <naty.lara@hotmail.com>
2022-01-03 18:40:49 -0300

    Add otimização inicialização

--- PMKStream/src/main/RunMKStreamParallel.java
+++ PMKStream/src/main/RunMKStreamParallel.java
@@ -6,6 +6,8 @@ import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -30,31 +32,51 @@ public class RunMKStreamParallel implements Runnable{
     private static String pushingType = "minnodes_groupstacks";
 
     public RunMKStreamParallel(String dirArquivoXML) {
-        this.dirArquivoXML = dirArquivoXML;
-        run();
+        //this.dirArquivoXML = dirArquivoXML;
+        //run();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //código para executar em paralelo
+               /* System.out.println("ID: " + Thread.currentThread().getId());
+                System.out.println("Nome: " + Thread.currentThread().getName());
+                System.out.println("Prioridade: " + Thread.currentThread().getPriority());
+                System.out.println("Estado: " + Thread.currentThread().getState());*/
+                String [] args = new String[6];
+                args[0] = "icde";
+                args[1] = "50000"; //50000 -> numero de queries
+                args[2] = "1"; //stacks
+                args[3] = "9";
+                args[5] = dirArquivoXML + "/"; //diretorio
+                main(args);
+            }
+        }).start();
     }
 
-    public static Runnable t1 = new Runnable() {
+    /*public static Runnable t1 = new Runnable() {
         public void run() {
             main();
         }
-    };
+    };*/
 
     public static void main(String ... args) {
-        System.out.println("RunMKStream1");
+        //System.out.println("RunMKStream1");
         String defaultDir = System.getProperty("user.dir");
         //System.out.println("base;#queries;#stacks;#experiment;#threads;");
         //System.out.println(defaultDir);
 
         //netbeans
-        args = new String[5];
+        /*
+        args = new String[6];
         args[0] = "isfdb";
-        args[1] = "5000"; //50000 -> numero de queries
+        args[1] = "50000"; //50000 -> numero de queries
         args[2] = "1"; //stacks
-        args[3] = "2"; //experimento
+        args[3] = "9"; //experimento
+
+        args[5] = "D:\\Computação\\TCC\\Código\\TCC_Final\\TCC_PMKStrem_Kafka\\PMKStream\\src\\xml\\datasets\\isfdb\\1\\";*/
 
         //lArgs[0] = defaultDir+"/xml/datasets/"+args[0].toLowerCase()+"/1/";
-        lArgs[0] = dirArquivoXML;
+        lArgs[0] = args[5];
         if(args[3].equals("1"))
             lArgs[1] = args[0].toLowerCase()+"_precision.txt";
         else if(args[3].equals("2"))
@@ -66,29 +88,33 @@ public class RunMKStreamParallel implements Runnable{
         else if(args[3].equals("5"))
             lArgs[1] = args[0].toLowerCase()+"_test_3l5t_50000.txt";
         else if(args[3].equals("6"))
-            lArgs[1] = args[0].toLowerCase()+"_test_0l2t_50000.txt";
+            lArgs[1] = args[0].toLowerCase()+"_original_6_tcc2_0l2t_50000.txt";
         else if(args[3].equals("7"))
             lArgs[1] = args[0].toLowerCase()+"_test_0l4t_50000.txt";
         else if(args[3].equals("8"))
             lArgs[1] = args[0].toLowerCase()+"_test_0l6t_50000.txt";
         else if(args[3].equals("9"))
-            lArgs[1] = args[0].toLowerCase()+"_test_simples.txt";
+            lArgs[1] = args[0].toLowerCase()+"_original_9_tcc2_0l2t_5000.txt";
+        else if(args[3].equals("10"))
+            lArgs[1] = args[0].toLowerCase()+"_original_10_tcc2_0l2t_25000.txt";
 
-        for(int i=0; i < 3; i++) {
-            registerState("Initiate", lArgs[1], defaultDir);
-            for(int j=1; j <= 8; j=j*2) {
+        String time = LocalDateTime.now().toString();
+        for(int i=1; i <= 10; i++) {
+            System.out.println("Inicio execução "+i+": " + lArgs[1] +" " + (new Date()).getTime());
+            registerState("Initiate", lArgs[1], defaultDir,time);
+            for(int j=8; j <= 8; j=j*2) {
                 lArgs[2] = processor;
                 lArgs[3] = args[0];
                 lArgs[4] = args[1];
                 lArgs[5] = memoryTime;
                 lArgs[6] = args[2];
                 lArgs[7] = pushingType;
-                //lArgs[8] = ""+j;
-                lArgs[8] = "1";
-                lArgs[9] = defaultDir + "/xml/queries/";
+                lArgs[8] = ""+j;
+                //lArgs[8] = "1";
+                lArgs[9] = defaultDir + "/src/xml/queries/";
 
-
-                System.out.println(lArgs[0]+";\t"+args[1]+";\t"+args[2]+";\t"+args[3]+";\t");
+               // System.out.println("Teste: "+lArgs[1]);
+                //System.out.println(lArgs[0]+";\t"+args[1]+";\t"+args[2]+";\t"+args[3]+";\t"+j+";\t");
 
                 try {
                     QueryProcessor.run((lArgs));
@@ -101,13 +127,15 @@ public class RunMKStreamParallel implements Runnable{
                 }
                 //System.out.println(Producer.getTotalTime());
             }
-            registerState("Terminate", lArgs[1], defaultDir);
+            registerState("Terminate", lArgs[1], defaultDir,time);
+            System.out.println("Fim execução "+i+": " + lArgs[1] +" " + (new Date()).getTime());
         }
     }
 
-    public static void registerState(String msg, String queryFileName, String defaultPath){
+    public static void registerState(String msg, String queryFileName, String defaultPath, String time){
         try {
-            BufferedWriter wr = new BufferedWriter(new FileWriter(defaultPath+"/xml/queries/results/time_"+queryFileName, true));
+            LocalDateTime localDateTime = LocalDateTime.now();
+            BufferedWriter wr = new BufferedWriter(new FileWriter(defaultPath+"/src/xml/queries/results/time_"+queryFileName, true));
             wr.write(msg, 0, msg.length());
             wr.newLine();
             wr.close();
